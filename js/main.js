1c50c69d4e397ab5be873a1f0eef30fc1ea533da
Mariia <m_shevchenko@fanshaweonline.ca>
2022-05-20 23:38:39 -0400

    Created the code with icons

--- js/main.js
+++ js/main.js
@@ -1,38 +1,15 @@
-// this is the module pattern 
-// also called an IIFE
-// an Immediately Invoked Fnction Expression
-
-
 (() => {
-    // This is a JavaScript comment
+
     console.log('fired! javascript is working!');
 
-    //Step1: go and find the elemnts on the page that you want the user to interact with
-
-    //querySelector takes a CSS selector and uses to find a match in the HTML file
-
-    // let theBadge = document.querySelector('#png'),
-    //     theVector = document.querySelector('#vector') ;
-    // querySelector finds the first element that matches
-
-    // querySelectorAll finds All elements that match the selector 
-    // and returns back an array-like object called a node list
-let theBadges = document.querySelectorAll ('img');
+    let theBadges = document.querySelectorAll ('img');
+    console.log(theBadges);
 
     function logMyId() {
-        debugger; // pauses code execution at this line
-
-        //log the element's ID to the console window
+        debugger;
         console.log(this.id);
-        //'this' refers to the element that you're interacting with at the moment
     }
 
-    //figure out how you want the user to interact with the badge graphic - a click, mouseover, mouseout, double click etc
-    // theBadge.addEventListener('click', logMyId);
-    // theVector.addEventListener('click', logMyId);
-
-    // for each badge in our collection, add an event handler
-    // arrow functions are just shorthand function declarations 
     theBadges.forEach(badge => badge.addEventListener ('click', logMyId));
 })();
 
